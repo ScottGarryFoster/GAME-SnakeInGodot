c7736f26e086bba82c85c3cf1f4533feb003d46e
Scott Foster <scottgarryfoster@googlemail.com>
2024-09-21 18:37:11 +0100

    Added the ability for the snake to grow

--- snake/Scenes/Main/Main.gd
+++ snake/Scenes/Main/Main.gd
@@ -9,31 +9,42 @@ extends Node
 @export_range(0, 100) var screenSizeY: int = 11
 @export_range(0, 100) var pieceSize: int = 40
 
-# Internals
-var playerPosition: Vector2i
-var player
+## All directions
 enum directions {Left, Right, Up, Down}
+
+## A reference to the player itself
+var player = []
+
+## Current player direction
 var playerDirection: directions
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	playerPosition = Vector2((screenSizeX / 2) * pieceSize, (screenSizeY / 2) * pieceSize)
-	
-	player = PlayerPieceScene.instantiate()
-	player.position = playerPosition
-	add_child(player)
+	player.append(PlayerPieceScene.instantiate())
+	player[0].position = Vector2((screenSizeX / 2) * pieceSize, (screenSizeY / 2) * pieceSize)
+	add_child(player[0])
 	
 	$MovementTimer.start()
-	pass # Replace with function body.
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	UpdatePlayerDirectionFromPlayerInput()
+
+	if Input.is_action_just_released("TestButton"):
+		doSpawn = true
 	pass
+	
+var doSpawn: bool
 
 func OnMovementTimerTimeout() -> void:
+	if doSpawn:
+		AddSnakePiece()
+		doSpawn = false
+		
+	ShiftSnake()
 	MovePlayerInCurrentDirection()
-	pass # Replace with function body.
+	pass
 	
 func UpdatePlayerDirectionFromPlayerInput():
 	if Input.is_action_pressed("MoveUp"):
@@ -49,20 +60,29 @@ func UpdatePlayerDirectionFromPlayerInput():
 func MovePlayerInCurrentDirection():
 	match playerDirection:
 		directions.Up:
-			playerPosition.y -= pieceSize
-			if playerPosition.y < 0:
-				playerPosition.y = screenSizeY * pieceSize
+			player[0].position.y -= pieceSize
+			if player[0].position.y < 0:
+				player[0].position.y = screenSizeY * pieceSize
 		directions.Down:
-			playerPosition.y += pieceSize
-			if playerPosition.y >= screenSizeY * pieceSize:
-				playerPosition.y = 0
+			player[0].position.y += pieceSize
+			if player[0].position.y >= screenSizeY * pieceSize:
+				player[0].position.y = 0
 		directions.Left:
-			playerPosition.x -= pieceSize
-			if playerPosition.x < 0:
-				playerPosition.x = screenSizeX * pieceSize
+			player[0].position.x -= pieceSize
+			if player[0].position.x < 0:
+				player[0].position.x = screenSizeX * pieceSize
 		directions.Right:
-			playerPosition.x += pieceSize
-			if playerPosition.x >= screenSizeX * pieceSize:
-				playerPosition.x = 0
-		
-	player.position = playerPosition
+			player[0].position.x += pieceSize
+			if player[0].position.x >= screenSizeX * pieceSize:
+				player[0].position.x = 0
+				
+func AddSnakePiece():
+	var newSnakeIndex = player.size()
+	player.append(PlayerPieceScene.instantiate())
+	player[newSnakeIndex].position = player[newSnakeIndex - 1].position
+	add_child(player[newSnakeIndex])
+	
+func ShiftSnake():
+	for i in range(len(player) - 1, -1, -1):
+		if(i > 0):
+			player[i].position = player[i - 1].position
